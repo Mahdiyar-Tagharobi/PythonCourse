fd889936440d654a98423b9f07abad47642f57f7
Mahdiyar Tagharobi <mahdiyar.ta00@gmail.com>
2024-02-05 22:47:12 +0330

    assignment1-1(calculator) was import from local pc to github repo

---
+++ assignment-1/assignment1-1.py
@@ -0,0 +1,52 @@
+import math
+
+print("----Simple Calculator----")
+
+print("--------\n + = sum\n - = sub\n * = mul\n / = dev\n f = fuctorial\n sin\n log\n sqrt\n cos\n tan\n cot\n --------")
+
+op = input("please enter your desire operator: ")
+
+if op == "+" or op == "-" or op == "*" or op == "/":
+    fn = float(input("enter first number"))
+    sn = float(input("enter second number"))
+else :
+    fn = float(input("enter the number"))
+
+if op == "+":
+    print(fn + sn)
+
+elif op == "-":
+    print(fn - sn)
+
+elif op == "*":
+    print(fn * sn)
+
+elif op == "/":
+    if sn == 0:
+        print("can't divide by zero")
+    else:
+        print(fn / sn)
+
+elif op == "sin":
+    print(math.sin(math.radians(fn)))
+
+elif op == "cos":
+    print(math.cos(math.radians(fn)))
+
+elif op == "tan":
+    print(math.tan(math.radians(fn)))
+
+elif op == "cot":
+    print(1 / math.tan(math.radians(fn)))
+
+elif op == "log":
+    print(math.log)
+
+elif op == "sqrt":
+    print(math.sqrt)
+
+elif op == "f":
+    print(math.factorial(int(fn)))
+
+else :
+    print("Your requested operator is not entered correctly")
