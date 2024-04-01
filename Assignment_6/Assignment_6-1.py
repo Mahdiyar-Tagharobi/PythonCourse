ffcb9bdf93ea82da35bca95d034b8a78da1584a8
Mahdiyar Tagharobi <mahdiyar.ta00@gmail.com>
2024-04-01 19:35:07 +0330

    Assignment 6-1 a little change again

--- Assignment_6/Assignment_6-1.py
+++ Assignment_6/Assignment_6-1.py
@@ -1,6 +1,8 @@
 import pyfiglet 
 import time 
 from colorama import Fore
+import random as r
+
 
 def plate_show():
     # Just cleaning
@@ -76,58 +78,114 @@ def timer(start_time):
 start_time = time.time()   
 
 greetings = pyfiglet.figlet_format("Tic Tac Toe", font="slant")
+ch_play = pyfiglet.figlet_format("Select mode : ", font="slant")
+
 print(greetings)
+print(ch_play)
+
 
 plate = [["-", "-", "-"],
          ["-", "-", "-"],
          ["-", "-", "-"]]
 
 plate_show()
+
 while True:
-    print("---Player 1 turn:")
-    n = 1
-    while True:
-        row = int(input("Enter row: "))
-        col = int(input("Enter column: "))
-        
-        if 0 <= row <= 2 and 0 <= col <= 2:
-            if plate[row][col] == "-":            
-                plate[row][col] = f"{Fore.RED}X{Fore.WHITE}"
-                plate_show()
-                if win(plate, n) == True:
-                    
-                    #print time
-                    timer(start_time)                    
-                    exit()
-                break
-            
-            else:
-                print("please select an empty cell :/")
-        else :
-                print("please select avalaible cell")
-            
-
-    print("---Player 2 turn:")
-    n = 2
-    while True:
-        row = int(input("Enter row: "))
-        col = int(input("Enter column: "))
-        
-        if 0 <= row <= 2 and 0 <= col <= 2:
-            if plate[row][col] == "-":            
-                plate[row][col] = f"{Fore.BLUE}O{Fore.WHITE}"
-                plate_show()
-                if win(plate, n) == True:
-                    
-                    #Calculating time
-                    timer(start_time)
-                    
-                    exit()
-                break
-            
-            else:
-                print("please select an empty cell :/")
-        else :
-                print("please select avalaible cell")
+    #select mode
+    mode = int(input("Enter 1 for 1 v 1 and enter 2 for PC v 1: "))
+    
+    if mode == 1:
+        while True:
+            print("---Player 1 turn:")
+            n = 1
+            while True:
+                row = int(input("Enter row: "))
+                col = int(input("Enter column: "))
                 
+                if 0 <= row <= 2 and 0 <= col <= 2:
+                    if plate[row][col] == "-":            
+                        plate[row][col] = f"{Fore.RED}X{Fore.WHITE}"
+                        plate_show()
+                        if win(plate, n) == True:
+                            
+                            #print time
+                            timer(start_time)                    
+                            exit()
+                        break
+                    
+                    else:
+                        print("please select an empty cell :/")
+                else :
+                        print("please select avalaible cell")
+                    
 
+            print("---Player 2 turn:")
+            n = 2
+            while True:
+                row = int(input("Enter row: "))
+                col = int(input("Enter column: "))
+                
+                if 0 <= row <= 2 and 0 <= col <= 2:
+                    if plate[row][col] == "-":            
+                        plate[row][col] = f"{Fore.BLUE}O{Fore.WHITE}"
+                        plate_show()
+                        if win(plate, n) == True:
+                            
+                            #Calculating time
+                            timer(start_time)
+                            
+                            exit()
+                        break
+                    
+                    else:
+                        print("please select an empty cell :/")
+                else :
+                        print("please select avalaible cell")
+    elif mode == 2:
+        while True:
+            print("---Player 1 turn:")
+            n = 1
+            while True:
+                row = int(input("Enter row: "))
+                col = int(input("Enter column: "))
+                
+                if 0 <= row <= 2 and 0 <= col <= 2:
+                    if plate[row][col] == "-":            
+                        plate[row][col] = f"{Fore.RED}X{Fore.WHITE}"
+                        plate_show()
+                        if win(plate, n) == True:
+                            
+                            #print time
+                            timer(start_time)                    
+                            exit()
+                        break
+                    
+                    else:
+                        print("please select an empty cell :/")
+                else :
+                        print("please select avalaible cell")
+                    
+
+            print("---Computer's 2 turn:")
+            n = Computer
+            
+            while True:
+                row = r.randint(0, 2)
+                col = r.randint(0, 2)
+                
+                if 0 <= row <= 2 and 0 <= col <= 2:
+                    if plate[row][col] == "-":            
+                        plate[row][col] = f"{Fore.BLUE}O{Fore.WHITE}"
+                        plate_show()
+                        if win(plate, n) == True:
+                            
+                            #Calculating time
+                            timer(start_time)
+                            
+                            exit()
+                        break
+                    
+                    else:
+                        print("please select an empty cell :/")
+                else :
+                        print("please select avalaible cell")                        
