15a0a390b5f999d31b10a7d1c174167d4d340e78
Mahdiyar Tagharobi <mahdiyar.ta00@gmail.com>
2024-04-01 05:38:52 +0330

    Assignment_6-1 change a little

--- Assignment_6/Assignment_6-1.py
+++ Assignment_6/Assignment_6-1.py
@@ -1,56 +1,80 @@
 import pyfiglet 
 import time 
-
+from colorama import Fore
 
 def plate_show():
+    # Just cleaning
+    print("\a")
+    
     for rows in plate:
         for cell in rows:
             print(cell, end=' ')
         print()    
+    
+    # Just cleaning
+    print("\a")
+
 
 def win(plate, n): 
+
     count = ""
-    #vertical
+    
+    # Text win used for if 
+    win_P1_text = '\x1b[31mX\x1b[37m' * 3
+    win_P2_text = '\x1b[34mO\x1b[37m' * 3
+    
+    # Vertical
     for i in range(3):
         for j in range(3):
             count  += plate[i][j]
              
-        if count == "OOO" or count == "XXX":
-            print(f"\n ----Player {n} wins :))) ")
+        if count == win_P1_text or count == win_P2_text :
+            print(f"-_-_Player {n} wins :))) ")
             return True
             break
             exit()
         else:
             draw = count
             count = ""
-    
-    #Horizontal
+    # Horizontal
     for i in range(3):
         for j in range(3):
             count  += plate[j][i] 
             
-        if count == "OOO" or count == "XXX":
-            print(f"\n ----Player {n} wins :))) ")
+        if count == win_P1_text or count == win_P2_text:
+            print(f"-_-_Player {n} wins :))) ")
             return True
             break
         else:
             draw = count
             count = ""
     
-    #Diagonal
-    if plate[0][0] + plate[1][1] + plate[2][2] == "XXX" or plate[0][2] + plate[1][1] + plate[2][0] == "XXX" or plate[0][0] + plate[1][1] + plate[2][2] == "OOO" or plate[0][2] + plate[1][1] + plate[2][0] == "OOO"  :
-        print(f"Player {n} wins :))) ")
+    # Diagonal
+    if plate[0][0] + plate[1][1] + plate[2][2] == win_P1_text or plate[0][2] + plate[1][1] + plate[2][0] == win_P1_text or plate[0][0] + plate[1][1] + plate[2][2] == win_P2_text or plate[0][2] + plate[1][1] + plate[2][0] == win_P2_text  :
+        print(f"-_-_Player {n} wins :))) ")
         return True
+    
+    # Draw
     else : 
         if  "-" not in draw:
             print("\n ----Draw :\ ")     
-    return False 
+    
+    # Still in process
+    return False  
 
+def timer(start_time): 
+    time_now = time.time()
+    t = int(time_now - start_time)
+    
+    m_and_s = t % 3600
 
-   
-    
-    
-    
+    s = m_and_s % 60
+
+    print(f"-_-_Elapsed time: {t // 3600} : {m_and_s // 60} : {s}\a")
+
+# Starting timer
+start_time = time.time()   
+
 greetings = pyfiglet.figlet_format("Tic Tac Toe", font="slant")
 print(greetings)
 
@@ -68,9 +92,12 @@ while True:
         
         if 0 <= row <= 2 and 0 <= col <= 2:
             if plate[row][col] == "-":            
-                plate[row][col] = "X"
+                plate[row][col] = f"{Fore.RED}X{Fore.WHITE}"
                 plate_show()
                 if win(plate, n) == True:
+                    
+                    #print time
+                    timer(start_time)                    
                     exit()
                 break
             
@@ -88,9 +115,13 @@ while True:
         
         if 0 <= row <= 2 and 0 <= col <= 2:
             if plate[row][col] == "-":            
-                plate[row][col] = "O"
+                plate[row][col] = f"{Fore.BLUE}O{Fore.WHITE}"
                 plate_show()
                 if win(plate, n) == True:
+                    
+                    #Calculating time
+                    timer(start_time)
+                    
                     exit()
                 break
             
