8ed5d28f0390580ff1b387a7b9c9adf9a11a7744
Mahdiyar Tagharobi <mahdiyar.ta00@gmail.com>
2024-05-17 17:45:31 +0330

    Assignment 8 p3

--- Assignment_8/Assignment_8.py
+++ Assignment_8/Assignment_8.py
@@ -4,10 +4,28 @@ def read_from_file():
 
     file = open("Assignment_8\Translate.txt", "r")
 
-    for line in file.readlines():
+    w = file.read().split("\n")
+    
+    for i in range(0, len(w), 2):
+        dic = {"en": w[i], "fa": w[i + 1]}
+        words.append(dic)
         
-        main_s = line.strip()
-        words.append(main_s)
     
     file.close()    
 
+def en_to_per():
+    ...
+
+def per_to_en():
+    ...
+
+def menu():
+    print("1- English to persian\n 2- Persian to English\n 3- Add new word to database\n 4- Exit")
+    
+
+
+
+inp = input("enter your sentence you want to translate it")
+
+for i in words:
+    ...
